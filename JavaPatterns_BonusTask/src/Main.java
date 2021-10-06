e589c71304d9c147fbd87c78e7ea8110c20d1a7b
alich <68386358+saylord@users.noreply.github.com>
2021-10-06 17:09:33 +0600

    Delete JavaPatterns_BonusTask directory

--- JavaPatterns_BonusTask/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-	    PizzaStore nyPizzaStore = new NYPizzaStore();
-	    Pizza pepperoniPizza = nyPizzaStore.orderPizza("pepperoni");
-    }
-}
--- JavaPatterns_BonusTask/src/Pizza.java
+++
@@ -1,8 +0,0 @@
-public abstract class Pizza {
-
-    public abstract void prepare();
-    public abstract void bake();
-    public abstract void cut();
-    public abstract void box();
-
-}
